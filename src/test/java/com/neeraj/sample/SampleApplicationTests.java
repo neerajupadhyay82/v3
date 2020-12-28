6353ba10b3bb530eac2ecd56bbcf786564e4163e
neerajupadhyay82 <neerajupadhyay82@gmail.com>
2020-12-28 16:00:41 +0530

    Add files via upload

---
+++ src/test/java/com/neeraj/sample/SampleApplicationTests.java
@@ -0,0 +1,11 @@
+package com.neeraj.sample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SampleApplicationTests {
+
+
+
+}
